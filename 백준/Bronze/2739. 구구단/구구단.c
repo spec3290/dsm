aa2503b745462efad29de2fb054d3fe803bb1c26
spec3290 <spec3290@gmail.com>
2025-10-10 21:41:32 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d", &a);
+    for(int i=1;i<=9;i++){
+        printf("%d * %d = %d\n",a,i,a*i);
+    }
+    return 0;
+}
